3176dd20fcdd786b05adde718169ac9bbd42a08d
hmt <hmt157@163.com>
2021-06-06 21:55:58 +0800

    idea test first commit

---
+++ src/main/java/org/hmt/git/GitTest.java
@@ -0,0 +1,11 @@
+package org.hmt.git;
+
+/**
+ * @author hmt
+ * @create 2021-06-06-21:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
